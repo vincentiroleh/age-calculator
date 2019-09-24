fa4c7e160fb9a7f0a2885680a3e4ef8ffc105cba
Vincent Iroleh <irolehiroleh@gmail.com>
2019-09-24 12:31:15 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: declare variables to store firstName, date of birth, and age.
+            string firstName;
+            DateTime birthDate;
+            TimeSpan age;
+
+            // TODO: Assign values to the variables from the users input
+            Console.WriteLine("Enter your First Name");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your date of birth using this format m/dd/yyyy");
+            birthDate = DateTime.Parse(Console.ReadLine()); //TODO: Use the Parse method to convert string to date formart
+
+            // TODO: minus date of birth from current date to find the actually age of the user
+            age = DateTime.Today - birthDate; 
+
+            /*TODO:
+            We welcomed the user by passing their name and telling them when they were born,
+            To get the actually age of the user, we couldn't use TimeSpans because years don't
+            have a constant number of days. Meaning that we're only able to get the total number
+            of days in the interval, so we divided days by 365.255 and then round them down.
+             */
+            Console.WriteLine($"Hi {firstName} you were born on {birthDate}");
+            Console.WriteLine("And you are {0} years old.", Math.Floor(age.Days/365.255));
+            Console.WriteLine("Thank you.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# C# Console Age Calculator
+
+A simple age calculator
+
+# Installation
+- Clone this REPO
+- Download [.NET](https://dotnet.microsoft.com/download)
+- Open this repo on Visual Studio or Visual Studio Code
+- If promote to install dependencies in VsCode, click yes
+- Press `CTRL+F5` OR type into your terminal `dotnet build && dotnet run` to run the program
+
+![](dotnetcalculator.gif)
+
+# Stack
+- C#
+- .NET
+
+# Author 
+Iroleh
